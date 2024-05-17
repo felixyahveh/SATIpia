5b528316b3b5e719b304b70a030cccb2a9b3f46b
felixyahveh <felixyahveh@gmail.com>
2024-05-17 00:05:39 -0600

    add(backend): configuración de proyecto

---
+++ Backend/cancionesApi/cancionesApi/Models/MusicaFavorita.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace cancionesApi.Models
+{
+	public class MusicaFavorita
+	{
+		public int Id { get; set; }
+		public string Cancion { get; set; }
+		public string Artista { get; set; }
+		public string IdUsuario { get; set; }
+	}
+}
+
---
+++ Backend/cancionesApi/cancionesApi/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace cancionesApi.Models
+{
+	public class Usuario
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public string Apellido { get; set; }
+		public string NombreUsuario { get; set; }
+		public string Contrasena { get; set; }
+	}
+}
+
